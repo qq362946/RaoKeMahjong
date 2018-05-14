1aad858f2b3769182181adad38da6d36a0b40167
Unknown <362946@qq.com>
2018-05-14 15:24:15 +0800

    搭建成功。unity项目从3D更改成2D项目。

---
+++ Server/Hotfix/Helper/MessageHelper.cs
@@ -0,0 +1,18 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+	public static class MessageHelper
+	{
+		public static void Broadcast(IActorMessage message)
+		{
+			Unit[] units = Game.Scene.GetComponent<UnitComponent>().GetAll();
+			ActorMessageSenderComponent actorMessageSenderComponent = Game.Scene.GetComponent<ActorMessageSenderComponent>();
+			foreach (Unit unit in units)
+			{
+				long gateSessionActorId = unit.GetComponent<UnitGateComponent>().GateSessionActorId;
+				actorMessageSenderComponent.GetWithActorId(gateSessionActorId).Send(message);
+			}
+		}
+	}
+}
